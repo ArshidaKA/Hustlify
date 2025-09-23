1917b7c0cc588ba8589810df9f7c8d661e658e78
Sahalumer <hfzsahalumer@gmail.com>
2025-09-23 11:08:15 +0530

    helmet added

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import AboutSection from './Components/AboutHero';
 import MeetOurTeam from './Components/MeetOurTeam';
 import AboutContent from './Components/AboutContent';
